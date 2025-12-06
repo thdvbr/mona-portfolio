4a59b6bdabb83ffe5058e2862603dcd539c7b6a9
thdvbr <soyoon.choi@code.berlin>
2025-12-06 22:13:31 +0100

    small margin tweaks

--- app/page.js
+++ app/page.js
@@ -28,26 +28,26 @@ return (
   <div className={styles.headerLeft}>
     MONA EL-BIRA 
       </div>
-        <a className={styles.headerEmail} href="mailto:monaelbira@gmail.com">CONTACT</a>
+        <a className={styles.headerEmail} href="mailto:mona.elbira@gmail.com">CONTACT</a>
         </div>
       <div>ARCHIVE PRODUCER & VISUAL RESEARCHER</div>
       <div>SELECTED PROJECTS ↓</div>
     </header>
       <section className={styles.grid}>
         <a href="https://charlottestreetfilms.com/" target="_blank">
-          <Image src="/01_SixBillionDollarMan.png" alt="Movie 1" width={400} height={450} className={styles.poster}/>
+          <Image src="/01_SixBillionDollarMan.png" alt="Six Billion Dollar Man" width={400} height={450} className={styles.poster}/>
         </a>
 
         <a href="https://www.riefenstahl-film.de/" target="_blank">
-          <Image src="/02_Riefenstahl.jpg" alt="Movie 2" width={400} height={450} className={styles.poster}/>
+          <Image src="/02_Riefenstahl.jpg" alt="Riefenstahl" width={400} height={450} className={styles.poster}/>
         </a>
 
         <a href="https://www.imdb.com/de/title/tt11687002/?reasonForLanguagePrompt=browser_header_mismatch" target="_blank">
-          <Image src="/03_Outrun.jpg" alt="Movie 3" width={400} height={450} className={styles.poster}/>
+          <Image src="/03_Outrun.jpg" alt="Outrun" width={400} height={450} className={styles.poster}/>
         </a>
 
         <a href="https://www.imdb.com/de/title/tt20420316/?reasonForLanguagePrompt=browser_header_mismatch" target="_blank">
-          <Image src="/04_Gladbeck.jpg" alt="Movie 4" width={400} height={450} className={styles.poster}/>
+          <Image src="/04_Gladbeck.jpg" alt="Gladbeck" width={400} height={450} className={styles.poster}/>
         </a>
       </section>
 
